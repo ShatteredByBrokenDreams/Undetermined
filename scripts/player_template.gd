e98f792f3540e97e867039ba29feef360988a180
SirMcSlushers <SirMcSlushy@gmail.com>
2026-09-18 13:07:44 -0400

    custom_res_stuff_example

--- scripts/player_template.gd
+++ scripts/player_template.gd
@@ -1,5 +1,4 @@
 extends Node2D
 
 
-@export var hp_max = 10
-@export var hp = 10
+@export var stats: Resource
---
+++ scripts/resources/stats_template.gd
@@ -0,0 +1,5 @@
+extends Resource
+class_name stats_template
+
+@export var hp_max = 10
+@export var hp = 10
